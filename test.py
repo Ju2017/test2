bad3eb2a607529e762ab733e5ee1a0f8910f30ee
Cornelli <gi004099@bisad.bisinfo.org>
2019-12-10 17:08:07 +0100

    Second commit

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 print('test')
+
+# git init
+# To add a folder
+# git remote set-url origin https://github.com/Ju2017/test2.git
+git add -A .
+git commit -m "Second commit"
+git push -u origin master
